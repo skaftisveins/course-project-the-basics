ddabea08849462354203ff789d63f025eb3d447f
ss1.h18@nemandi.ntv.is <ss1.h18@nemandi.ntv.is>
2019-03-01 20:08:25 +0000

    Components % Databinding (no service)

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,14 +6,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://i.imgur.com/pIan4G8.jpg'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://i.imgur.com/pIan4G8.jpg')
+    new Recipe('Alien 1979', 'First Movies', 'https://i.imgur.com/pIan4G8.jpg'),
+    new Recipe('Aliens 1986', 'Second Movie', 'https://i.ebayimg.com/images/g/jiEAAOSwHMJYKvHx/s-l300.jpg'),
+    new Recipe('Alien 3 1992', 'Third Movie', 'https://bmoviesbreviewed.files.wordpress.com/2015/04/alien-poster.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
